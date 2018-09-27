f3b2be2e202362f54b719e94d243a93cf858c019
saitejdandge <saitejdandge1@gmail.com>
2018-09-27 16:50:12 +0530

    chages

---
+++ mater.py
@@ -0,0 +1,10 @@
+print("Hello welcome to Pixel, Image classification as service")
+
+choice=input("1.Create New Project\n2.Use Exisisting Project")
+
+if choice==1:
+	createProject()
+	pass
+if choice==2:
+	exsistingProject()
+	pass
