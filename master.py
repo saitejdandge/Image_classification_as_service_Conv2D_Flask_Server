a29dc1880bee75e20e383169476db4fa0175cc43
saitejdandge <saitejdandge1@gmail.com>
2018-09-27 16:47:31 +0530

    changes

---
+++ master.py
@@ -0,0 +1,4 @@
+print("Hello welcome to Pixel, Image classification as service")
+print("1.Create New Project")
+print("2.Use Exisisting Project")
+choice=input()
--- server/server.py
+++ server/server.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/createproject")
 def createproject():
-    return {project_id:"this is sample project id"}
+    return 'project_id %s' % "randomproject_id"
+
+
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return 'User %s' % username
+
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return 'Post %d' % post_id
+
 
 if __name__ == "__main__":
     app.run()
