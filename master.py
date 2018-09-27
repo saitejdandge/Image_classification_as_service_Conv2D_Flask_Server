8313830612ed4598783846d017ed9b4229e989d0
saitejdandge <saitejdandge1@gmail.com>
2018-09-27 17:28:24 +0530

    changes

--- master.py
+++ master.py
@@ -1,4 +1,42 @@
-print("Hello welcome to Pixel, Image classification as service")
-print("1.Create New Project")
-print("2.Use Exisisting Project")
-choice=input()
+import os
+import json
+from google_images_download import google_images_download
+print("Hello welcome to Pixel, Image classification as service\n\n")
+
+
+#choice=input("1.Create New Project\n2.Use Existing Project\n");
+
+#rest call
+def createDataset():
+	record_count=int(input("Enter number of classes"))
+	records=""
+	count=0
+	for x in range(0,record_count):
+		records=records+input("enter keyword \n")
+		if count!=record_count-1:
+			records=records+","
+			pass
+		pass
+	response = google_images_download.googleimagesdownload()
+	arguments = {"keywords":records,"limit":10} #creating list of arguments
+	paths = response.download(arguments)   #passing the arguments to the function
+	
+	pass
+
+#rest call with project_name 
+def createProject():
+
+	project_name=input("Please enter project name\n")
+	os.mkdir(project_name)
+	os.chdir(project_name)
+	createDataset()
+	pass
+
+createProject()
+
+# if choice=="1":
+# 	createProject()
+# 	pass
+# if choice=="2":
+# 	existing()
+# 	pass
--- mater.py
+++ mater.py
@@ -1,10 +1,21 @@
+import os
+
 print("Hello welcome to Pixel, Image classification as service")
 
-choice=input("1.Create New Project\n2.Use Exisisting Project")
 
-if choice==1:
+choice=input("1.Create New Project\n2.Use Existing Project")
+print("chosen choice is "+choice)
+def createProject():
+
+	project_name=input("Please enter project name")
+	os.mkdir(project_name)
+
+	pass
+
+
+if choice=="1":
 	createProject()
 	pass
-if choice==2:
-	exsistingProject()
+if choice=="2":
+	existing()
 	pass
